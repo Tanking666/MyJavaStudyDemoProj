c41cd1f3140e22be19626667bccead87f9506bdb
Tanking666 <437351312@qq.com>
2019-12-13 15:32:55 +0800

    Rename Dir

--- spring4_crm/src/com/itheima/dao/CustomerDao.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.dao;
-
-import com.itheima.domain.Customer;
-
-public interface CustomerDao {
-	public void save(Customer customer);
-}
--- spring4_crm/src/com/itheima/service/CustomerService.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.service;
-
-import com.itheima.domain.Customer;
-
-public interface CustomerService {
-	public void save(Customer customer);
-}
